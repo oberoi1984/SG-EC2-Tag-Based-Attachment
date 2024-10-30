68ed539f7968ebcbe57f6f0a5453baad4c25f829
oberoi1984 <41862558+oberoi1984@users.noreply.github.com>
2024-10-30 14:53:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,17 +15,17 @@ data "aws_instances" "target_instances" {
   }
 }
 
-data "aws_network_interface" "instance_eni" {
-  count = length(data.aws_instances.target_instances.ids)
-  filter {
+data "aws_network_interface" "instance_eni" {                         # Data block to retrieve network interfaces attached to each running instance identified by the specified tags.
+  count = length(data.aws_instances.target_instances.ids)             # Using count to dynamically create multiple entries based on the number of instances found.
+  filter {                                                            # Filter to find network interfaces attached to each instance by its ID.
     name   = "attachment.instance-id"
-    values = [data.aws_instances.target_instances.ids[count.index]]
+    values = [data.aws_instances.target_instances.ids[count.index]]   # Accesses each instance ID found in the "target_instances" data block using count.index.
   }
 }
 
-resource "aws_network_interface_sg_attachment" "attach_sg" {
-  count              = length(data.aws_instances.target_instances.ids)
-  security_group_id  = var.security_group_id
-  network_interface_id = data.aws_network_interface.instance_eni[count.index].id
+resource "aws_network_interface_sg_attachment" "attach_sg" {                       # Resource block to attach a security group to each network interface found for the specified instances.
+  count              = length(data.aws_instances.target_instances.ids)             # The count is used to iterate over each network interface found.
+  security_group_id  = var.security_group_id                                       # Specifies the security group ID to attach, passed in as a variable.
+  network_interface_id = data.aws_network_interface.instance_eni[count.index].id   # Specifies the ID of the network interface for each instance, using the count index to match.
 }
 
